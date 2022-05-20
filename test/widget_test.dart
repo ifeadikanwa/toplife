f5170803d840ed78ac3ae7a96e249e607ea66a2b
ifeadikanwa <ezeifyyy@gmail.com>
2022-05-20 14:55:57 -0400

    setup basic folder structure updated path to main.dart in vscode launch configurations

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:toplife/main.dart';
+import 'package:toplife/core/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
