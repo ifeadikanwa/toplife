160675946e2ef1e3a6d77441b7f36e2ed35d958e
ifeadikanwa <40605740+ifeadikanwa@users.noreply.github.com>
2022-07-13 14:37:14 -0400

    fixed code analysis problem

--- lib/main_systems/system_school/domain/usecases/school_usecases.dart
+++ lib/main_systems/system_school/domain/usecases/school_usecases.dart
@@ -1,12 +1,4 @@
-import 'package:toplife/main_systems/system_school/data/repository/school_repositories.dart';
 
 class SchoolUsecases {
-  final SchoolRepositories _schoolRepositories;
-
-  const SchoolUsecases({
-    required SchoolRepositories schoolRepositories,
-  }) : _schoolRepositories = schoolRepositories;
-
-
-      
+    
 }
